ac0817bf0fa62d68e782f168949fd1c10f827ac0
Serhii Zinchenko <ser.zin12@gmail.com>
2023-03-06 14:17:19 +0200

    completed tasks 1 - 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,87 @@
 'use strict'
 
-const cityInput = document.getElementById('city-input');
-const cityList = document.getElementById('city-list');
+const input = document.getElementById('city');
+const button = document.getElementById('btn');
+const cityList = document.getElementById('cityList');
+let chart = null;
 
-cityInput.addEventListener('input', function () {
-    const query = this.value;
-    if (query.length > 0) {
-        const XHR = new XMLHttpRequest();
-        XHR.open('GET', `http://api.openweathermap.org/geo/1.0/direct?q=${query}&appid=8144ea5ad0a23d10dc61228957515eee`);
-        XHR.onload = function () {
-            if (XHR.status === 200) {
-                const data = JSON.parse(XHR.responseText);
-                let results = '';
-                data.forEach(city => {
-                    results += `<li><button>${city.name}</button></li>`;
+input.addEventListener('input', function () {
+    const inputValue = input.value;
+    if (inputValue.length > 0) {
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=20&appid=8144ea5ad0a23d10dc61228957515eee`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const cities = data.map(city => city.name);
+                const uniqueCities = [...new Set(cities)];
+                cityList.innerHTML = '';
+                uniqueCities.forEach(city => {
+                    const li = document.createElement('li');
+                    li.textContent = city;
+                    li.addEventListener('click', function () {
+                        input.value = city;
+                        cityList.innerHTML = '';
+                    });
+                    cityList.prepend(li);
                 });
-                cityList.innerHTML = results;
-            } else {
-                console.error(XHR.statusText);
-            }
-        };
-        XHR.onerror = function () {
-            console.error(XHR.statusText);
-        };
-        XHR.send();
-    } else {
+            })
+            .catch(error => console.log(error));
+    }
+    else {
         cityList.innerHTML = '';
     }
 });
+
+button.addEventListener('click', function () {
+    const selectedCity = input.value;
+    if (chart) {
+        chart.destroy();
+    }
+    getChartData(selectedCity);
+});
+
+function getChartData(selectedCity) {
+    const apiKey = '8144ea5ad0a23d10dc61228957515eee';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${apiKey}&units=metric`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('.weather-city').textContent = selectedCity;
+            document.querySelector('.weather-description').textContent = data.weather[0].description;
+            document.querySelector('.weather-temp').textContent = `${data.main.temp.toFixed()}°C`;
+
+            const currentDay = new Date().toISOString().slice(0, 10);
+            const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&appid=${apiKey}&units=metric`;
+            fetch(forecastUrl)
+                .then(response => response.json())
+                .then(data => {
+                    const temps = data.list.map(item => item.main.temp);
+                    const hours = data.list.map(item => new Date(item.dt_txt).getHours());
+
+                    chart = new Chart(document.querySelector('#chart'), {
+                        type: 'line',
+                        data: {
+                            labels: hours,
+                            datasets: [
+                                {
+                                    label: 'Температура',
+                                    data: temps,
+                                    backgroundColor: 'rgba(0, 0, 255, 0.1)',
+                                    borderColor: 'blue',
+                                    borderWidth: 1
+                                }
+                            ]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    });
+                })
+                .catch(error => console.error(error));
+        })
+        .catch(error => console.error(error));
+}
